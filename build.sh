ecc806ecb60c8242434de0dfa859540105479922
srich009 <srich009@ucr.edu>
2019-01-08 03:00:37 -0800

    trying to figure out where the dfvisotor travels

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 # Compiler build and test script
 
-#----------------------------------------
+#========================================
+# MAIN
+#========================================
 
 # Global Variables
 JTB_JAR="../stuff/jtb.jar"          # Tree Builder
@@ -11,30 +13,28 @@ VAPOR_I="../stuff/vapor.jar"        # Vapor Interpreter
 VAPOR_P="../stuff/vapor-parser.jar" # Vapor Parser
 MARS="../stuff/mars.jar"            # MIPS Interpreter
 
-#----------------------------------------
-
-PROJ="p1" # phase1
-
 # Driver for user subroutines, called at bottom
 function main()
 {
-    # set -e
-    # init_cc
-    test_cc
-    clean_cc
+    init_p1
+    check_p1
+    clean_p1
 }
 
-#----------------------------------------
+#========================================
+# PHASE 1 - Type Checking
+#========================================
 
 # Create skeleton MiniJava parser and generate syntax tree classes and visitor classes, etc...
-function init_cc()
+function init_p1()
 {
-    if ( test -d $PROJ ) ; then
+    if ( test -d p1 ) ; then
         echo "Phase1 build exists"
         return
     fi
-    mkdir $PROJ
-    cd $PROJ
+    echo "Creating initial Phase1 build"
+    mkdir p1
+    cd p1
     java -jar $JTB_JAR $MINI_J
     javacc jtb.out.jj
     touch Typecheck.java
@@ -42,26 +42,27 @@ function init_cc()
 }
 
 # testing type checking
-function test_cc()
+function check_p1()
 {
-    cd $PROJ
-    echo "Compiling"
+    P1_FILE="../stuff/Factorial.java"
+    cd p1
+    echo "Compiling Phase1"
     javac Typecheck.java
-    echo "Testing"
-    java Typecheck ../stuff/Factorial.java
+    echo "Checking "$P1_FILE
+    java Typecheck $P1_FILE
     cd ..
 }
 
 # remove class files
-function clean_cc()
+function clean_p1()
 {
-    cd $PROJ;      rm *.class
+    cd p1;         rm *.class
     cd visitor;    rm *.class; cd ..
     cd syntaxtree; rm *.class; cd ..
     cd ..
 }
 
-#----------------------------------------
+#========================================
 
 # Call main driver function
 main
