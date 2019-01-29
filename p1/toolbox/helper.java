5a49f9931ec5e646f9ab903b8d3b64c31d3790d2
Krishna Pakalapati <krishna@krishnas-mbp.home>
2019-01-28 20:43:08 -0800

    working symbol table

--- p1/toolbox/helper.java
+++ p1/toolbox/helper.java
@@ -1,6 +1,37 @@
 package toolbox;
+import java.sql.Struct;
 import java.util.*;
 
+import struct.ClassStruct;
+import struct.FuncStruct;
+
 public class helper{
+
+    public static String classname(ClassStruct c) {
+        return c.getName();
+    }
+
+    public static String methodname(FuncStruct f) {
+        return f.getName();
+    }
+
+    public static boolean distinct(Vector<String> l) {
+        boolean duplicates = false; 
+        for(String i: l) {
+            for(String j: l) {
+                if(i == j) {
+                    duplicates = true;
+                }
+            }
+        }
+
+        return duplicates;
+    }
+
+    public static Vector<Struct> fields(ClassStruct c) {
+        Vector<Struct> f = new Vector<Struct>();
+        f.addAll(c.getFields());
+        return f;
+    }
     
 }
