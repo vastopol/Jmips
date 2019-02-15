48d2334fb70283f1a8ecfaace3083b419f690893
srich009 <srich009@ucr.edu>
2019-02-15 00:36:51 -0800

    super awesome build script patched for phase2

--- tests/tester.java
+++ tests/tester.java
@@ -2,8 +2,12 @@ class Tester
 {
     public static void main(String[] args)
     {
+        int val;
         A as;
         as = new A();
+
+        // val = as.f(1,2);
+        // System.out.println(val);
 
         // as.x = 1;    // ERROR
         // System.out.println(as.x);
@@ -20,23 +24,8 @@ class A
     {
         int c;
         c = b + a;
-        return 5;   // broke if returns a literal
-    }
-
-    public boolean setX(int y)
-    {
-        x = y;
-        return true;   // broke if returns a literal
+        // return 5;   // broken if returns a literal
+        return c;
     }
 }
 
-class B extends A
-{
-    int k;
-
-    public boolean setK(int w)
-    {
-        k = w;
-        return true;   // broke if returns a literal
-    }
-}
