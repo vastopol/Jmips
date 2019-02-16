d3b6ff83cf22d83a5f6170302c99a23dc1427cbd
Krishna Pakalapati <krishna@krishnas-mbp.home>
2019-02-16 15:15:41 -0800

    array assignment, length, lookup, and allocation working

--- tests/tester.java
+++ tests/tester.java
@@ -8,9 +8,13 @@ class A {
 	public int run() {
 		int[] a;
 		int b;
+		int c;
 		a = new int[20];
 		b = a.length;
-		a[4] = 7;
-		return a[4];
+		// a[4] = 7;
+		// c = 0 - 1;
+		a[4] = 8;
+		c = a[4];
+		return c;
 	}
 }
