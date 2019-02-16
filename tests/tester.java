2a8c6e81df231ef129017e27c30be5fced1e90bf
Krishna Pakalapati <krishna@krishnas-mbp.home>
2019-02-15 22:48:49 -0800

    merged and wiped

--- tests/tester.java
+++ tests/tester.java
@@ -6,28 +6,11 @@ class Main {
 
 class A {
 	public int run() {
-<<<<<<< HEAD
-		//System.out.println(42);
 		int[] a;
 		int b;
 		a = new int[20];
 		b = a.length;
-		return 99;
-=======
-		int a;
-		int b;
-		a = this.helper(12);
-		b = this.helper(15);
-		return a + b;
-	}
-
-	public int helper(int param) {
-		int x;
-		x = param;
-		param = param + 1;
-		System.out.println(x);
-		return x;
->>>>>>> master
+		return b;
 	}
 }
 
